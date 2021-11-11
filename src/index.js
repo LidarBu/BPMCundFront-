a2cdb327987cf0f27cdc0cfa0f9c3b766492161b
Lidar <lidarbuzaglo@gmail.com>
2021-11-11 14:16:24 +0200

    c before m

--- src/index.js
+++ src/index.js
@@ -1,14 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom/cjs/react-router-dom.min";
+import BPMPage from "./operationPage/BPMPage";
+import logo from "./Assests/logo.jpg";
+import "./Fonts/Lato/Lato-Regular.ttf";
+
+const NavBar = () => {
+  return (
+    <>
+      <div className="topnav">
+        <img className="logo" src={logo} alt="" width="70" height="81" />
+        <h2>Pais BPM Administrator</h2>
+      </div>
+    </>
+  );
+};
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <div className="main">
+    <NavBar></NavBar>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={BPMPage}></Route>
+        <Route exact path="/bp" component={BPMPage}></Route>
+      </Switch>
+    </Router>
+  </div>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/operationPage/BPMPage.js
@@ -0,0 +1,151 @@
+import conf from "../Assests/Configuration.json";
+import React, { useState } from "react";
+import "./BPMPage.css";
+import Popup from "reactjs-popup";
+
+async function FetchLog(bpm) {
+  var url = new URL(conf.BACKEND_SERVER + "/getlogtest");
+  var params = { bpm: bpm };
+  url.search = new URLSearchParams(params).toString();
+  console.log("fetch func:" + bpm);
+  console.log(url.toString());
+  const response = await fetch(url)
+    .then((res) => {
+      console.log(res);
+      return res.json();
+    })
+    .catch((err) => {
+      console.log(err);
+      return { data: [], error: err };
+    });
+
+  return response;
+}
+
+function OpenPopup(message, func1, func2) {
+  console.log("message pop " + message);
+  var state = true;
+  return (
+    <Popup
+      open={state}
+      modal
+      closeOnDocumentClick
+      onClose={() => {
+        func1(false);
+        func2(false);
+      }}
+    >
+      <span> messagegee mf </span>
+    </Popup>
+  );
+}
+
+const LogButton = (props) => {
+  const [state, setState] = React.useState(false);
+  const [data, setData] = React.useState([[]]);
+
+  const onClickHandler = () => {
+    FetchLog(props.value)
+      .then((res) => res.data)
+      .then((res) => {
+        setData(res);
+      })
+      .then(setState(true));
+  };
+
+  return (
+    <>
+      <button
+        id={props.value}
+        className="button_a button_b"
+        onClick={() => onClickHandler()}
+      >
+        Last Logs
+      </button>
+      <Popup
+        open={state}
+        modal
+        closeOnDocumentClick
+        onClose={() => setState(false)}
+      >
+        <div className="popup-content">
+          {data.map((elem) => {
+            console.log(elem);
+            return <p className="logs">{elem.query}</p>;
+          })}
+        </div>
+      </Popup>
+    </>
+  );
+};
+
+const RestartButton = (props) => {
+  const [state, setState] = React.useState(false);
+  return (
+    <>
+      <button
+        id={props.value}
+        className="button_a button_b"
+        onClick={() => {
+          console.log("restart clicked");
+          setState(!state);
+        }}
+      >
+        Restart
+      </button>
+      {state ? OpenPopup("Restart", setState) : null}
+    </>
+  );
+};
+
+function BPMQuery(props) {
+  const { name, port, description, state } = props.bpm;
+  const trimedName = name.substring(3, 6); //trim the bpm num only.
+  const stateClass = (state) => {
+    if (state === "WORKING") {
+      return "green_state";
+    } else {
+      return "red_state";
+    }
+  };
+
+  return (
+    <div>
+      <div className={stateClass(state)} />
+      <p className="inline">{name}</p>
+      <p className="inline">Port: {port}</p>
+      <p className="inline">Desc: {description}</p>
+
+      <LogButton value={trimedName} className="inline" />
+      <RestartButton value={trimedName} className="inline" />
+    </div>
+  );
+}
+
+function BPMPage() {
+  const [bpms, setObj] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch(conf.BACKEND_SERVER + "/gettest")
+      .then((response) => {
+        response.json().then((res) => {
+          console.log(res);
+          res.data.forEach((element) => {
+            setObj((prev) => [...prev, element]);
+            // console.log(element);
+          });
+        });
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <div class="container">
+      {bpms.map((elem) => {
+        return <BPMQuery key={elem.name} bpm={elem}></BPMQuery>;
+      })}
+    </div>
+  );
+}
+
+export default BPMPage;
